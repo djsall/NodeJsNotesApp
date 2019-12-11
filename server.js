43f6697a46d8635e40e5b76fc7dfeb04a733bcd5
Proksa Levente <proksalevente@gmail.com>
2019-12-11 19:24:00 +0100

    formatting

--- server.js
+++ server.js
@@ -6,52 +6,60 @@ const mysql = require('mysql');
 const fileLocation = __dirname + '/public/';
 
 const con = mysql.createConnection({
-	host: 'localhost',
-	user: 'root',
-	password: '',
-	database: 'test'
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'test'
 });
 
 app.use(bodyParser.json());
 app.use(express.static(fileLocation));
 
 app.get('/', (req, res) => {
-	res.sendFile(fileLocation + 'index.html');
+  res.sendFile(fileLocation + 'index.html');
 });
 
 app.get('/getlist', (req, res) => {
-	queryMsg('select * from shopping where ip="' + req.ip + '"').then(doneResult => {
-		res.send(JSON.stringify(doneResult));
-	});
+  queryMsg('select * from shopping where ip="' + req.ip + '"').then(
+    doneResult => {
+      res.send(JSON.stringify(doneResult));
+    }
+  );
 });
 
 app.get('/clearlist', (req, res) => {
-	command('delete from shopping where ip="' + req.ip + '"');
-	res.sendStatus(200);
+  command('delete from shopping where ip="' + req.ip + '"');
+  res.sendStatus(200);
 });
 
 app.post('/postItem', (req, res) => {
-	command('insert into shopping (ip, message) values ("' + req.ip + '", "' + req.body.message + '")');
-	res.sendStatus(200);
+  command(
+    'insert into shopping (ip, message) values ("' +
+      req.ip +
+      '", "' +
+      req.body.message +
+      '")'
+  );
+  res.sendStatus(200);
 });
 
 function command(query) {
-	con.query(query, (err) => {
-		if (err) throw err;
-	})
+  con.query(query, err => {
+    if (err) throw err;
+  });
 }
 
 function queryMsg(query) {
-	return new Promise((resolve, reject) => {
-		let doneResult = [];
-		con.query(query, (err, result) => {
-			if (err) reject(err);
-			result.forEach(item => {
-				let curr = item['message'];
-				doneResult.push(curr);
-			});
-			resolve(doneResult);
-		});
-	});
+  return new Promise((resolve, reject) => {
+    let doneResult = [];
+    con.query(query, (err, result) => {
+      if (err) reject(err);
+      result.forEach(item => {
+        let curr = item['message'];
+        doneResult.push(curr);
+      });
+      resolve(doneResult);
+    });
+  });
 }
-app.listen(3000, "192.168.0.136");
+app.listen(3000, '192.168.0.136');
